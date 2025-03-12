bc95c732a0df82dd1129279254ec106c353a776b
kieran.white <kieranwhite84@gmail.com>
2025-03-12 14:54:36 +0000

    Change autosan middleware to apply each filter in given rules array to each field

--- src/Services/SanitizrService.php
+++ src/Services/SanitizrService.php
@@ -2,6 +2,8 @@
 
 namespace AnomanderRevan\Sanitizr\Services;
 
+use Illuminate\Support\Facades\Log;
+
 class SanitizrService
 {
     protected array $filters;
@@ -11,14 +13,20 @@ class SanitizrService
         $this->filters = $filters;
     }
 
-    public function sanitize(array $data, array $rules): array
+    public function sanitize(array $data, array $filters): array
     {
-        foreach ($rules as $field => $fieldFilters) {
-            if (isset($data[$field])) {
-                foreach ($fieldFilters as $filter) {
-                    if (isset($this->filters[$filter])) {
-                        $data[$field] = call_user_func($this->filters[$filter], $data[$field]);
+        if (!empty($filters) && !empty($data)) {
+            foreach ($filters as $filter) {
+                if (isset($this->filters[$filter])) {
+                    if (is_callable($this->filters[$filter])) {
+                        foreach ($data as $key => $value) {
+                            $data[$key] = call_user_func($this->filters[$filter], $value);
+                        }
+                    } else {
+                        Log::error("Filter '$filter' is not callable");
                     }
+                } else {
+                    Log::error("Filter '$filter' is not defined");
                 }
             }
         }
